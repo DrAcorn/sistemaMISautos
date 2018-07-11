0e03df6598871af5d4d5231cbdc1dfb3837fe2ac
Destroy-Youth <luis.herrerae@outlook.com>
2018-07-11 14:42:11 -0500

    Botstrap

---
+++ misAutos/misAutos/src/main/java/org/unitec/misAutos/MisAutosApplication.java
@@ -0,0 +1,12 @@
+package org.unitec.misAutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisAutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisAutosApplication.class, args);
+	}
+}
